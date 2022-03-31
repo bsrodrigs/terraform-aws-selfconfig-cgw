c20128f0c22f4cdc0bdb930b74279fc6472b6ad8
Bruno Rodrigues <brenatosr@gmail.com>
2022-03-31 23:05:21 +0100

    Description improvements

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "blue_vpc" {
   value       = module.blue_vpc
-  description = "VPC outputs for blue side. For more details see official documentation from Terraform registry https://registry.terraform.io/modules/terraform-aws-modules/vpc/aws/latest?tab=outputs"
+  description = "Blue side VPC outputs. For more details see official documentation https://registry.terraform.io/modules/terraform-aws-modules/vpc/aws/latest?tab=outputs"
 }
 
 output "green_vpc" {
   value       = module.green_vpc
-  description = "VPC outputs for green side. For more details see official documentation from Terraform registry https://registry.terraform.io/modules/terraform-aws-modules/vpc/aws/latest?tab=outputs"
+  description = "Green side VPC outputs. For more details see official documentation https://registry.terraform.io/modules/terraform-aws-modules/vpc/aws/latest?tab=outputs"
 }
